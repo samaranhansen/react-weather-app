14316142621beb4cb8f9b28d26e484a0a552ad5d
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2024-02-21 12:38:19 -0600

    Added root elements and bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <div className="footer">
+        <p>
+          <a
+            href="https://github.com/samaranhansen/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          written by{" "}
+          <a
+            href="https://www.linkedin.com/in/samara-hansen-452501168/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Samara Hansen
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
