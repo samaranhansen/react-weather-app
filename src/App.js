8a17d0a2356c2e5a21c55b34d7e610681b9268e7
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2024-02-22 12:28:19 -0600

    Added search form, inputs, and configured

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ export default function App() {
     <div className="App">
       <div className="weatherContainer">
         <div className="searchContainer">
-          <form className="searchForm"></form>
+          <form className="searchForm">
+            <input
+              type="search"
+              placeholder="Search for a city here"
+              className="searchInput"
+            />
+            <input type="submit" value="Search" className="searchButton" />
+          </form>
         </div>
       </div>
       <div className="footer">
