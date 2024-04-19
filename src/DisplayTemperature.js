7ac5026f90241849e20804ef40a587d4fec2316c
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2024-04-19 08:04:25 -0500

    Added unit conversion functions

--- src/DisplayTemperature.js
+++ src/DisplayTemperature.js
@@ -2,7 +2,37 @@ import React, { useState } from "react";
 
 export default function DisplayTemperature(props) {
   const [unit, setUnit] = useState("°C");
-  let temperature = Math.round(props.temperature);
+  let celcius = Math.round(props.temperature);
+  let fahrenheit = (celcius * 9) / 5 + 32;
+
+  function toFahrenheit(event) {
+    event.preventDefault();
+  }
+
+  if (unit === "°C") {
+    setUnit("°F");
+    return (
+      <div className="temperatureDisplay">
+        <h1>
+          {fahrenheit}°
+          <button className="unitButton" onClick={toFahrenheit}>
+            {unit}
+          </button>
+        </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="temperatureDisplay">
+        <h1>
+          {fahrenheit}°
+          <button className="unitButton" onClick={changeUnit}>
+            {unit}
+          </button>
+        </h1>
+      </div>
+    );
+  }
 
   function changeUnit(props) {
     setUnit("°F");
@@ -11,7 +41,7 @@ export default function DisplayTemperature(props) {
   return (
     <div className="temperatureDisplay">
       <h1>
-        {temperature}°
+        {celcius}°
         <button className="unitButton" onClick={changeUnit}>
           {unit}
         </button>
