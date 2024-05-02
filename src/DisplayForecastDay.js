3fe554527352edea9986d2951344f4e0d68eba6b
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2024-05-01 22:32:21 -0500

    Changed colors of fonts

--- src/DisplayForecastDay.js
+++ src/DisplayForecastDay.js
@@ -2,31 +2,41 @@ import React from "react";
 
 export default function DisplayForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.temp.max);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.data.temp.min);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
 
   function day() {
-    let date = new Date(props.data.dt * 1000);
+    let date = new Date(props.data.time * 1000);
     let day = date.getDay();
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     return days[day];
   }
 
   return (
     <div className="forecastDisplay">
-      <div className="forecastDay">{day}</div>
-      <img src={props.data.daily[0].condition.icon_url} alt="description" />
+      <div className="forecastDay">{day()}</div>
+      <img
+        src={props.data.condition.icon_url}
+        alt="description"
+        className="forecastIcon"
+      />
       <div className="forecastTemps">
-        <span className="forecastTempMin">{maxTemperature()}</span>{" "}
-        <span className="forecastTempMax">{minTemperature()}</span>
+        <span className="forecastTempMin">{minTemperature()}</span>{" "}
+        <span className="forecastTempMax">{maxTemperature()}</span>
       </div>
     </div>
   );
